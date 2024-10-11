cd84e6ad446616fb4a3d731a7a35fd91097cc638
Jose Esparza <pebeto99@gmail.com>
2024-10-11 13:13:29 -0500

    Adding ZSH config

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,7 +6,7 @@ if wezterm.configbuilder then
     config = wezterm.config_builder()
 end
 
-config.font = wezterm.font("JuliaMono Nerd Font Mono")
+config.font = wezterm.font("JuliaMono")
 config.font_size = 11.0
 
 config.color_scheme = "Grayscale (dark) (terminal.sexy)"
